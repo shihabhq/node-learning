371c6cb620b01b6ae92c827fd527b5b84e0e6ca9
shihab <adnansaim15@yahoo.com>
2024-07-15 06:51:30 +0600

    modern js export import syntax example

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,5 +1,20 @@
 const generateRandNum = (max = 100, min = 1) => {
+  // * you can export from here if you want
   return Math.floor(Math.random() * max) + min;
 };
 
-module.exports = generateRandNum;
+const add = (...args) => {
+  let i = 0;
+  let sum = 0;
+  while (i < args.length) {
+    sum = sum + args[i];
+    i++;
+  }
+  return sum;
+};
+
+// *note:you can export one as default and some others as object
+
+export { generateRandNum }; //!you need to export an object
+
+export default add;
