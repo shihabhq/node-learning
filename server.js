5cc16299c86195daf22d38d065defeef061bb325
shihab <adnansaim15@yahoo.com>
2024-07-15 08:48:39 +0600

    advanced routing added

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import http from "http";
+import fs from "fs/promises";
+
+//NOTE: in esmodules you have the __dirname and __filename variable
+//but in modernjs you don't have that feature. so we need the url
+import url from "url";
+import path from "path";
+
+///get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server1 = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
@@ -8,30 +18,31 @@ const server1 = http.createServer((req, res) => {
 
 let port = process.env.PORT;
 
-const server2 = http.createServer((req, res) => {
-    try{
-        if(req.method === 'GET'){
-            if (req.url === "/") {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end("<h1>hwllo this is homepage</h1>");
-              } else if (req.url === "/about") {
-                res.writeHead(200, { "Content-Type": "text/html" });
-                res.end("<h1>hwllo this is about page</h1>");
-              } else {
-                res.writeHead(404, { "Content-Type": "text/html" });
-                res.end(`<h1>We did not find the ${req.url.replace("/", "")} page</h1>`);
-              }
-        }else{
-            res.writeHead(405, { "Content-Type": "text/html" });
-            res.end(`<h1>Method not allowed</h1>`);
-        }
-    }catch(e){
-        res.writeHead(500, { "Content-Type": "text/html" });
-            res.end(`<h1>server problem occured</h1>`);
+const server2 = http.createServer(async (req, res) => {
+  try {
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        res.statusCode = 404;
+        filePath = path.join(__dirname, "public", "notfound.html");
+      }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      res.writeHead(405, { "Content-Type": "text/html" });
+      res.end(`<h1>Method not allowed</h1>`);
     }
-
-
-  
+  } catch (e) {
+    res.writeHead(500, { "Content-Type": "text/html" });
+    res.end(`<h1>server problem occured</h1>`);
+  }
 });
 
 server2.listen(port, () => {
